32ffd3eb2ca021b9d20ef7d8bc8cb050f7c9cb88
lizziek <lizzieking521@gmail.com>
2018-09-13 22:55:55 +1000

    initial setup and structure

---
+++ backend/lib/index.js
@@ -0,0 +1,14 @@
+
+'use strict'
+
+class tripMemesData {
+
+    tripAdvisorAnyData() 
+    {
+        let dankMemes = 'DankEztMemEz';
+        return dankMemes;
+    }
+    
+}
+module.exports = new tripMemesData();
+
